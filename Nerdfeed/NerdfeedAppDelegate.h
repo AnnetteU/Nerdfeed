fbe4df9c6a46edc1638446b922ed98ec6c441537
Annette Undheim <annetteundheim@Annettes-MacBook-Pro.local>
2013-12-18 22:23:10 +0100

    Initial Commit

---
+++ Nerdfeed/NerdfeedAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  NerdfeedAppDelegate.h
+//  Nerdfeed
+//
+//  Created by Annette Undheim on 18/12/13.
+//  Copyright (c) 2013 Annette Undheim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NerdfeedAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Nerdfeed/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Nerdfeed
+//
+//  Created by Annette Undheim on 18/12/13.
+//  Copyright (c) 2013 Annette Undheim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "NerdfeedAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([NerdfeedAppDelegate class]));
+    }
+}
